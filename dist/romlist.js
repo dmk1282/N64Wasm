ba340f7f3e12d92c40bbc32b62516109782cd26a
dmk1282 <58492185+dmk1282@users.noreply.github.com>
2024-01-12 02:45:09 -0500

    Update romlist.js

--- dist/romlist.js
+++ dist/romlist.js
@@ -1,6 +1,6 @@
 var ROMLIST = [
     /*
-    {url:"roms/rom1.z64",title:"Game 1"},
+    {url:"https://archive.org/download/game_20240111/game.z64",title:"Game 1"},
     {url:"roms/rom2.v64",title:"Game 2"},
     {url:"roms/rom3.v64",title:"Game 3"},
     */
